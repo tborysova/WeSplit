eb7b163d94eb81b1e06a3ccd2c667799fecaf34f
tborysova <borysova.tetiana@gmail.com>
2022-11-11 16:08:24 +0100

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Tetiana Borysova on 11.11.22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
